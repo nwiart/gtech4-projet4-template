607a5fff78f4dc168a9367ab9d0b089808ea10be
Gotvna <nvogt@gaming.tech>
2024-12-18 12:03:56 +0100

    Brick Generation

--- Game/GameManager.cpp
+++ Game/GameManager.cpp
@@ -2,6 +2,7 @@
 
 #include "Scene/Scene.h"
 #include "Scene/GameObject.h"
+#include <iostream>
 
 GameManager &GameManager::getInstance()
 {
@@ -26,20 +27,21 @@ void GameManager::run()
 	paddle->getComponent<RectComponent>().setColor(sf::Color::Green);
 	paddle->getComponent<RectComponent>().setSize(sf::Vector2f(150, 20));
 
-	GameObject* ball = m_scene->instantiateObject();
+	GameObject *ball = m_scene->instantiateObject();
 	addComponents<CircleSystem, RenderSystem, PhysicsSystem>(*ball);
 	ball->setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
 	ball->getComponent<CircleComponent>().setColor(sf::Color::Red);
 	ball->getComponent<CircleComponent>().setRadius(10);
 	ball->getComponent<PhysicsComponent>().setVelocity(sf::Vector2f(300.0F, 300.0F));
-	ball->getComponent<PhysicsComponent>().setCollideObjectCallback([](GameObject& objA, PhysicsComponent& collA, GameObject& objB, PhysicsComponent& collB) -> bool {
+	ball->getComponent<PhysicsComponent>().setCollideObjectCallback([](GameObject &objA, PhysicsComponent &collA, GameObject &objB, PhysicsComponent &collB) -> bool
+																	{
 		sf::Vector2f v = collA.getVelocity();
 		v.y = -v.y;
 		v.x = 10.0F * (((objA.getPosition().x + 6.0F) - (objB.getPosition().x + 75.0F)));
 		collA.setVelocity(v);
-		return true;
-	});
-	ball->getComponent<PhysicsComponent>().setCollideScreenCallback([](GameObject& objA, PhysicsComponent& collA, int side) -> bool {
+		return true; });
+	ball->getComponent<PhysicsComponent>().setCollideScreenCallback([](GameObject &objA, PhysicsComponent &collA, int side) -> bool
+																	{
 		sf::Vector2f v = collA.getVelocity();
 		switch ((PhysicsSystem::ScreenSide) side)
 		{
@@ -49,8 +51,43 @@ void GameManager::run()
 		case PhysicsSystem::ScreenSide::BOTTOM: break;
 		}
 		collA.setVelocity(v);
-		return true;
-	});
+		return true; });
+
+	const sf::Vector2f brickSize(100.0f, 30.0f);
+	const float spacing = 10.0f;
+	const float startX = (window.getSize().x - (brickSize.x * 4 + spacing * 3)) / 2;
+	const float startY = 50.0f;
+
+	GameObject *brick = m_scene->instantiateObject();
+	addComponents<RenderSystem, RectSystem>(*brick);
+	brick->setPosition(sf::Vector2f(200, 100));
+	brick->getComponent<RectComponent>().setColor(sf::Color::Blue);
+	brick->getComponent<RectComponent>().setSize(brickSize);
+
+	std::vector<GameObject *> bricks;
+
+	 for (int row = 0; row < 2; ++row) {
+		for (int col = 0; col < 4; ++col) {
+			std::cout << "Creating brick at row " << row << ", col " << col << std::endl;
+
+			float posX = startX + col * (brickSize.x + spacing);
+			float posY = startY + row * (brickSize.y + spacing);
+
+			GameObject* brick = m_scene->instantiateObject();
+			if (!brick) {
+				std::cerr << "Failed to create brick at row " << row << ", col " << col << std::endl;
+				continue;
+			}
+
+			addComponents<RenderSystem, RectSystem>(*brick);
+			brick->setPosition(sf::Vector2f(posX, posY));
+			brick->getComponent<RectComponent>().setColor(sf::Color::Blue);
+			brick->getComponent<RectComponent>().setSize(brickSize);
+			std::cout << brick->getID() << std::endl;
+
+			bricks.push_back(brick);
+		}
+	}
 
 	while (window.isOpen())
 	{
@@ -78,30 +115,6 @@ void GameManager::run()
 				paddle->setPosition(paddle->getPosition() + sf::Vector2f(PADDLE_SPEED, 0));
 			}
 		}
-
-		/*if (ball->getPosition().x <= 0 || ball->getPosition().x + 20 >= window.getSize().x)
-		{
-			ballVelocity.x = -ballVelocity.x;
-		}
-		if (ball->getPosition().y <= 0)
-		{
-			ballVelocity.y = -ballVelocity.y;
-		}
-
-		sf::FloatRect paddleBounds(paddle->getPosition().x, paddle->getPosition().y, paddle->getComponent<RectComponent>().getSizeX(), paddle->getComponent<RectComponent>().getSizeY());
-		sf::FloatRect ballBounds = ball->getComponent<CircleComponent>().getShape()->getGlobalBounds();
-
-		if (paddleBounds.intersects(ballBounds))
-		{
-			ballVelocity.y = -ballVelocity.y;
-		}
-
-		if (ball->getPosition().y > window.getSize().y)
-		{
-			ball->setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
-			ballVelocity = sf::Vector2f(3.0f, -3.0f);
-		}*/
-
 
 		g_systems.getSystem<RectSystem>().update(0);
 		g_systems.getSystem<CircleSystem>().update(0);
