528811373f1c5215c14bfaa823666fcc362bf5a5
Gotvna <nvogt@gaming.tech>
2024-12-18 10:17:14 +0100

    add setter to circle and rect

--- Game/Component/CircleComponent.h
+++ Game/Component/CircleComponent.h
@@ -9,6 +9,9 @@ public:
 
     sf::Shape* getShape() { return &m_shape; }
 
+    inline void setColor(sf::Color color) { m_shape.setFillColor(color); }
+    inline void setRadius(int radius) { m_shape.setRadius(radius); }
+    inline void setTexture(sf::Texture(text)) { m_shape.setTexture(&text); }
 private:
     sf::CircleShape m_shape;
 };
